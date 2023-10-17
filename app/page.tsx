f916acdb12b012497c92c68f82d496d3ce4055e3
Roko <rokoponjarac2@gmail.com>
2023-10-17 18:23:11 +0200

    refactor remove text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Ej kako si</h1>
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
