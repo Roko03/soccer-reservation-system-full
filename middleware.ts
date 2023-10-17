eb7005dba441992b8848c1d87fb83febe29a70fb
Roko <rokoponjarac2@gmail.com>
2023-10-17 18:58:22 +0200

    make middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,48 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { NextMiddleware, NextFetchEvent } from 'next/server';
 
-export default createMiddleware({
-    // A list of all locales that are supported
-    locales: ['en', 'hr'],
+const locales = ['en', 'hr'];
+const publicPages = ["/", "/about"];
+const PUBLIC_FILE = /\.(.*)$/
 
-    // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-    defaultLocale: 'en'
-});
+export default withExtraMiddleware(
+    createMiddleware({
+        locales: locales,
+        defaultLocale: 'en'
+    })
+);
+
+function withExtraMiddleware(next: NextMiddleware) {
+    return async (request: NextRequest, event: NextFetchEvent) => {
+        if (
+            request.nextUrl.pathname.startsWith('/_next') ||
+            request.nextUrl.pathname.includes('/api/') ||
+            PUBLIC_FILE.test(request.nextUrl.pathname)
+        ) {
+            return
+        }
+
+        const url = request.nextUrl.clone();
+        let token = request.cookies.get('token');
+        const locale = request.cookies.get('NEXT_LOCALE')?.value || 'en';
+        const isPublicPage = url.pathname === `${locale === 'en' ? '/' : '/hr'}`
+
+        if (token && url.pathname === `${locale === 'en' ? '/' : '/hr/'}authentication`) {
+            url.pathname = `${locale === 'en' ? '/' : '/hr'}`;
+            return NextResponse.redirect(url)
+        }
+
+        if (!token && isPublicPage) {
+            url.pathname = `${locale === 'en' ? '/' : '/hr/'}authentication`;
+            return NextResponse.redirect(url)
+        }
+
+        return next(request, event);
+    };
+}
 
 export const config = {
-    // Skip all paths that should not be internationalized. This example skips
-    // certain folders and all pathnames with a dot (e.g. favicon.ico)
-    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+    matcher: ['/((?!_next|.*\\..*).*)']
 };
