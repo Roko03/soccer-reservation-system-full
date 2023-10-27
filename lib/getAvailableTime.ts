6a64012de21ae142a0ae4babed3d84fb98bcd266
Roko <rokoponjarac2@gmail.com>
2023-10-27 13:52:44 +0200

    refactor registration system

--- lib/getAvailableTime.ts
+++ lib/getAvailableTime.ts
@@ -3,9 +3,24 @@ export default async function getAvailableTimes(stadionId: string, date: string)
 
     const times = ['17:00', "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
 
-    if (!response.ok) throw new Error(`Can't fetch`);
+    const yy = new Date().getFullYear();
+    const mm = new Date().getMonth() + 1;
+    const dd = new Date().getDate();
 
+    const todayDate = `${yy}-${mm}-${dd}`;
+
+    if (!response.ok) throw new Error(`Can't fetch`);
     const data: Reservation[] = await response.json()
+
+    if (todayDate === date) {
+        const getRealTime = new Date().getHours();
+        const realTime = `${getRealTime}:00`;
+        const index = times.indexOf(realTime);
+
+        if (index >= 0) {
+            times.splice(0, index + 1);
+        }
+    }
 
     const filterByDates = data.filter(sd => sd.startDate === date);
 
