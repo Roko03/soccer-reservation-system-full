5cdca9884be9112313ac1679e463f02f399b9679
Roko <rokoponjarac2@gmail.com>
2023-10-31 18:32:14 +0100

    add stadium name to reservation

--- app/[locale]/profile/components/profile-reservation/components/reservation-box/ProfileReservationBoxComponent.tsx
+++ app/[locale]/profile/components/profile-reservation/components/reservation-box/ProfileReservationBoxComponent.tsx
@@ -1,6 +1,8 @@
 import ButtonComponent from "@/app/[locale]/components/button/ButtonComponent";
 import styles from "./ProfileReservationBoxComponent.module.scss";
 import { useTranslations } from "next-intl";
+import getStadium from "@/lib/getStadium";
+import { useEffect, useState } from "react";
 
 interface ProfileReservationBoxComponentProps {
   reservation: ReservationMongo;
@@ -11,13 +13,25 @@ const ProfileReservationBoxComponent: React.FC<
   ProfileReservationBoxComponentProps
 > = ({ reservation, deleteReservation }) => {
   const t = useTranslations("Index");
+  const [stadiumName, setStadiumName] = useState<string>("");
+
+  const getStadiumName = async (id: string) => {
+    const stadium: Stadium = await getStadium(id);
+    setStadiumName(stadium.name);
+  };
+
+  useEffect(() => {
+    getStadiumName(reservation.stadiumId);
+  }, []);
+
   return (
     <div className={styles.reservation_box}>
       <div className={styles.reservation_box__description}>
         <p>{reservation.startDate}</p>
         <span>-</span>
         <p>{reservation.time}</p>
-        <p>{reservation.stadiumId}</p>
+        <span>-</span>
+        <p>{stadiumName}</p>
       </div>
       <ButtonComponent
         variant={"delete"}
